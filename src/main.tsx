bf3467cfb56d3bb8018ed7b03c09f9bbb9380804
NemBaboulinet <schwarzadler@hotmail.fr>
2026-07-29 11:49:43 +0200

    feat: Vercel analytics instead GA

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import App from './App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Layout from '../src/components/Layout.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<Layout>
-			<App />
-		</Layout>
-	</StrictMode>,
+	<>
+		<StrictMode>
+			<Layout>
+				<App />
+			</Layout>
+		</StrictMode>
+		<Analytics />
+	</>,
 );
